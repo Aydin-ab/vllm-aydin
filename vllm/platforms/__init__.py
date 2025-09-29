ac65865bb8d60604d45102a0df6c0e3dd9b81b87
Aydin Abiar <aydin@anyscale.com>
2025-09-28 22:54:23 -0700

    apply hotfix
    Signed-off-by: Aydin Abiar <aydin@anyscale.com>

--- vllm/platforms/__init__.py
+++ vllm/platforms/__init__.py
@@ -7,7 +7,8 @@ from typing import TYPE_CHECKING, Optional
 
 from vllm import envs
 from vllm.plugins import load_plugins_by_group
-from vllm.utils import resolve_obj_by_qualname, supports_xccl
+from vllm.utils import (_is_benchmark_command, resolve_obj_by_qualname,
+                        supports_xccl)
 
 from .interface import _Backend  # noqa: F401
 from .interface import CpuArchEnum, Platform, PlatformEnum
@@ -215,6 +216,16 @@ def resolve_current_platform_cls_qualname() -> str:
             activated_builtin_plugins[0]]()
         logger.info("Automatically detected platform %s.",
                     activated_builtin_plugins[0])
+    elif _is_benchmark_command():
+        # For 'vllm bench *' commands: use CPU instead of UnspecifiedPlatform
+        # to avoid errors during CLI init
+        platform_cls_qualname = "vllm.platforms.cpu.CpuPlatform"
+        logger.warning(
+            "No platform detected while running benchmark commands, "
+            "but defaults to CPU Platform instead of UnspecifiedPlatform "
+            "to avoid errors during CLI init. "
+            "This may be expected when running your benchmark commands on "
+            "CPU-only machines (Ray cluster head node for example) ")
     else:
         platform_cls_qualname = "vllm.platforms.interface.UnspecifiedPlatform"
         logger.info(
