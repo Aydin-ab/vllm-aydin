db72cd3f531d776091db182ac1a29f46d8380c26
Aydin Abiar <aydin@anyscale.com>
2025-09-30 10:56:33 -0700

    apply hotfix: simply check argv
    Signed-off-by: Aydin Abiar <aydin@anyscale.com>

--- vllm/platforms/__init__.py
+++ vllm/platforms/__init__.py
@@ -1,14 +1,14 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 import logging
+import sys
 import traceback
 from itertools import chain
 from typing import TYPE_CHECKING, Optional
 
 from vllm import envs
 from vllm.plugins import load_plugins_by_group
-from vllm.utils import (_is_benchmark_command, resolve_obj_by_qualname,
-                        supports_xccl)
+from vllm.utils import resolve_obj_by_qualname, supports_xccl
 
 from .interface import _Backend  # noqa: F401
 from .interface import CpuArchEnum, Platform, PlatformEnum
@@ -216,7 +216,7 @@ def resolve_current_platform_cls_qualname() -> str:
             activated_builtin_plugins[0]]()
         logger.info("Automatically detected platform %s.",
                     activated_builtin_plugins[0])
-    elif _is_benchmark_command():
+    elif sys.argv[1] == "bench":
         # For 'vllm bench *' commands: use CPU instead of UnspecifiedPlatform
         # to avoid errors during CLI init
         platform_cls_qualname = "vllm.platforms.cpu.CpuPlatform"
